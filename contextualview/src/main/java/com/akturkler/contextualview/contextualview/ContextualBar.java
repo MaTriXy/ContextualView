6f8a2cf48cc7c535db8cd276a3d92289b41c730e
Emre Akturk <emreaktrk@hotmail.com>
2014-04-13 21:36:14 +0300

    Initial commit

---
+++ contextualview/src/main/java/com/akturkler/contextualview/contextualview/ContextualBar.java
@@ -0,0 +1,23 @@
+package com.akturkler.contextualview.contextualview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by emreakturk on 13/04/14.
+ */
+public class ContextualBar extends LinearLayout {
+
+    public ContextualBar(Context context) {
+        super(context);
+    }
+
+    public ContextualBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ContextualBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
